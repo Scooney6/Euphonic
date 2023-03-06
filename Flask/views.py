4008a8aadacf6da2fb948a45a3323b4c2dfb25ae
Aman Baig <amanbaig@siliconchips.com>
2023-03-05 23:38:56 -0500

    Compare Page Complete
    Compare Page Almost Complete
    Minor tweaking is needed.

--- Flask/views.py
+++ Flask/views.py
@@ -8,18 +8,19 @@ from Flask import app
 def index():
     return render_template("index.html")
 
-# LOGOUT
+# # Logout
 # @app.route('/logout', methods=['POST'])
 # def logout():
 #     logout_user()
 #     return redirect(url_for('index'))
 
 
-
 # Home Page
 @app.route("/home", methods=["POST", "GET"])
 def home():
-    username = "John"  # Testcase
+    # Testcase
+    username = "John"
+    # Code
     return render_template("home.html", username=username)
 
 
@@ -30,5 +31,41 @@ def compare():
     username = "John"
     friendsUsername = "Chris"
     comparison_score = 80
+    artist_1 = "Drake"
+    artist_2 = "Olivia Rodrigo"
+    artist_3 = "The Weeknd"
+    artist_4 = "Taylor Swift"
+    artist_5 = "Morgan Wallen"
+    track_1 = "Flowers"
+    trackArtist_1 = "Miley Cyrus"
+    track_2 = "Kill Bill"
+    trackArtist_2 = "SZA"
+    track_3 = "Boy's A Liar, Pt. 2"
+    trackArtist_3 = "PinkPantheress & Ice Spice"
+    track_4 = "Creepin'"
+    trackArtist_4 = "Metro Boomin, The Weeknd & 21 Savage"
+    track_5 = "Last Night"
+    trackArtist_5 = "Morgan Wallen"
+
     # Code
-    return render_template("compare.html", username=username, comparison_score=comparison_score, friendsUsername=friendsUsername)
+    return render_template(
+        "compare.html",
+        username=username,
+        comparison_score=comparison_score,
+        friendsUsername=friendsUsername,
+        artist_1=artist_1,
+        artist_2=artist_2,
+        artist_3=artist_3,
+        artist_4=artist_4,
+        artist_5=artist_5,
+        track_1=track_1,
+        trackArtist_1=trackArtist_1,
+        track_2=track_2,
+        trackArtist_2=trackArtist_2,
+        track_3=track_3,
+        trackArtist_3=trackArtist_3,
+        track_4=track_4,
+        trackArtist_4=trackArtist_4,
+        track_5=track_5,
+        trackArtist_5=trackArtist_5
+    )
