0aa3aef2a8a78341f5485a5b315e409c071e24b8
Pedro101s <optmistic269@gmail.com>
2023-04-03 17:28:30 -0400

    Added queries

--- Database/db.py
+++ Database/db.py
@@ -16,3 +16,41 @@ def getUsers():
         cur = con.cursor()
         cur.execute("SELECT Username FROM User")
         return cur.fetchall()
+
+def getUser(username):
+    with connect() as con:
+        cur = con.cursor()
+        cur.execute("SELECT Username FROM User WHERE Username = %s", (username,))
+        user = cur.fetchone()
+        return user[0] if user else None
+
+def authorize(username, password):
+    with connect() as con:
+        cur = con.cursor()
+        cur.execute("SELECT Username FROM User WHERE Username = %s AND Password = %s", (username, password))
+        user = cur.fetchone()
+        if user:
+            return True
+        else:
+            return False
+
+def getToken(username):
+    with connect() as con:
+        cur = con.cursor()
+        cur.execute("SELECT Spotifytoken FROM User WHERE Username = %s", (username,))
+        token = cur.fetchone()
+        return token[0] if token else None
+
+def adduser(username, password):
+    with connect() as con:
+        cur = con.cursor()
+        cur.execute("INSERT INTO User (Username, Password) VALUES (%s, %s)", (username, password))
+        con.commit()
+        return True
+
+def updateToken(username, token):
+    with connect() as con:
+        cur = con.cursor()
+        cur.execute("UPDATE User SET Spotifytoken = %s WHERE Username = %s", (token, username))
+        con.commit()
+        return True
